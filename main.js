8469ffb37cf381318cc703fee50c5e48d0b0d677
Barron Mckeon <barronmckeon@Barrons-MacBook-Pro.local>
2022-02-03 14:54:17 -0500

    rev-1

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("HH   HH        lll lll                                  lll      dd !!!")
+console.log("HH   HH   eee  lll lll  oooo   ww      ww  oooo  rr rr  lll      dd !!!")
+console.log("HHHHHHH ee   e lll lll oo  oo  ww      ww oo  oo rrr  r lll  dddddd !!!")
+console.log("HH   HH eeeee  lll lll oo  oo   ww ww ww  oo  oo rr     lll dd   dd !!!")
+console.log("HH   HH  eeeee lll lll  oooo     ww  ww    oooo  rr     lll  dddddd !!!")
+console.log(" ")
+
+
+
+
+
+
+
+                                                                                                                               
+
+console.log("SSS   t           t       h      GGG          l   ")  
+console.log("S      t           t       h     G             l   ")  
+console.log(" SSS  ttt rrr eee ttt  ccc hhh   G  GG ooo  aa l  ss") 
+console.log("    S  t  r   e e  t  c    h  h  G   G o o a a l  s ") 
+console.log("SSSS   tt r   ee   tt  ccc h  h   GGG  ooo aaa l ss ") 
+                                                     
+                                                     
+
+
+
